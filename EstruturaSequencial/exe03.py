2b52e0df5ecab8501c4bc553e97e2494908c7279
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2021-05-06 18:36:04 -0300

    Exercícios 02 a 05

---
+++ EstruturaSequencial/exe03.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+
+soma = num1 + num2
+
+print(f'A soma dos número informados é: {soma}')
---
+++ EstruturaSequencial/exe05.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que converta metros para centímetros."""
+
+metro = float(input('Informe a distância em metros: '))
+
+cm = int(metro * 100)
+
+print(f'A distância informada em cm é : {cm} cm.')
