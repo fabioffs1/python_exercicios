ae3e8f1087a1edc5c760e332e949f02c1ce2d19e
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2025-01-18 23:43:08 -0300

    Delete exe_06.py

--- exe_06.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-raio_circulo = int(input('Informe o raio do círculo: '))
-pi = 3.14
-area_circulo = pi * raio_circulo ** 2
-print(f'Área do círculo: {area_circulo:.2f}')
