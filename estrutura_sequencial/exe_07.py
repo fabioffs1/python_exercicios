f5aa315d61e843fa78d50787dbe8a4b31e815378
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2025-01-19 00:04:26 -0300

    exe 07

---
+++ estrutura_sequencial/exe_07.py
@@ -0,0 +1,6 @@
+# Faça um Programa que calcule a área de um quadrado,
+# em seguida mostre o dobro desta área para o usuário.
+lado = int(input('informe o lado do quadrado: '))
+area = lado * lado
+dobro_area = area * 2
+print(f'Dobro da área: {dobro_area}')
