88db0acbc13497d5af84d77f9da97474d0ad72cf
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2025-01-18 23:42:54 -0300

    Delete exe_05.py

--- exe_05.py
+++
@@ -1,4 +0,0 @@
-# Faça um Programa que converta metros para centímetros.
-metros = int(input('Informe o valor em metros: '))
-centimetros = metros * 100
-print(f'Valor informado em centímetros: {centimetros} cm')
