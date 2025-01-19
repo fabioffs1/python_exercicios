60473a2b5d61bd574cb21a7201ce90520d1feb5e
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2025-01-18 23:41:57 -0300

    Delete exe_02.py

--- exe_02.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem 
-# O número informado foi [número].
-
-numero = int(input('Informe um número: '))
-print(f'O número informado foi: {numero}')
