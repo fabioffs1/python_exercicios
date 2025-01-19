57ad10dcc6d9c41df5c9d0bf46e316a7d3c88785
Fábio Fernandes <ffs.fernandes1992@gmail.com>
2025-01-18 23:42:27 -0300

    Delete exe_03.py

--- exe_03.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça dois números e imprima a soma.
-num_1 = int(input("Informe o primeiro número: "))
-num_2 = int(input("Informe o segundo número: "))
-soma = num_1 + num_2
-print(f'A soma dos números é: {soma}')
